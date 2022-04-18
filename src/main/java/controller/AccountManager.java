1cd1bdeb2f338cdaf0254a7298ddb4bff123fb72
q1e123 <robertnicolaesolca@gmail.com>
2022-04-18 17:14:51 +0300

    added acc manager

---
+++ src/main/java/controller/AccountManager.java
@@ -0,0 +1,33 @@
+package controller;
+
+import model.UserEntity;
+
+import javax.xml.crypto.Data;
+
+public class AccountManager {
+    private static AccountManager accountManager;
+
+    public static AccountManager getInstance() {
+        if (AccountManager.accountManager == null){
+            accountManager = new AccountManager();
+        }
+        return accountManager;
+    }
+
+    public void createNewAccount(String username, String password){
+        UserEntity user = new UserEntity();
+        user.setUsername(username);
+        try {
+            user.setPassword(password);
+        } catch (InsecurePasswordException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public UserEntity getLoggedUser(String username, String password){
+        DatabaseManager databaseManager = DatabaseManager.getInstance();
+        String hashedPassword = SecurityManager.getSHA256(password);
+        UserEntity user = databaseManager.getUserEntityLogin(username, hashedPassword);
+        return null;
+    }
+}
