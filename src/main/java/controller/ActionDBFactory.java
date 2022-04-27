dcdc3fca330e37d954e52f4f2a5174c2bebddfb1
q1e123 <robertnicolaesolca@gmail.com>
2022-04-27 10:53:20 +0300

    added addSendKeys

--- src/main/java/controller/ActionDBFactory.java
+++ src/main/java/controller/ActionDBFactory.java
@@ -6,9 +6,20 @@ import model.ActionEntity;
 public class ActionDBFactory {
     public static void addGoTo(Integer routineId, String url){
         DatabaseManager databaseManager = DatabaseManager.getInstance();
-        ActionEntity actionEntity = new ActionEntity("GoTo", routineId);
+        ActionEntity actionEntity = new ActionEntity("GO_TO", routineId);
         databaseManager.add(actionEntity);
         ActionArgumentEntity actionArgumentEntity = new ActionArgumentEntity("URL", url, actionEntity.getId());
         databaseManager.add(actionArgumentEntity);
     }
+
+    public static void addSendKeys(Integer routineId, String xPath, String keys){
+        DatabaseManager databaseManager = DatabaseManager.getInstance();
+        ActionEntity actionEntity = new ActionEntity("SEND_KEYS", routineId);
+        databaseManager.add(actionEntity);
+        ActionArgumentEntity actionArgumentEntityXPath = new ActionArgumentEntity("xPath", xPath, actionEntity.getId());
+        ActionArgumentEntity actionArgumentEntityKeys = new ActionArgumentEntity("KEYS", keys, actionEntity.getId());
+        databaseManager.add(actionArgumentEntityXPath);
+        databaseManager.add(actionArgumentEntityKeys);
+    }
+
 }
