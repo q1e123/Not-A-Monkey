5aa8267d530fbc843f6511e87475f0e1bde30a5b
q1e123 <robertnicolaesolca@gmail.com>
2022-04-27 09:10:06 +0300

    modified sha to be easier to read

--- src/main/java/controller/CryptoManager.java
+++ src/main/java/controller/CryptoManager.java
@@ -1,12 +1,16 @@
 package controller;
 
+import com.google.common.hash.HashCode;
+import com.google.common.hash.HashFunction;
 import com.google.common.hash.Hashing;
 
 import java.nio.charset.StandardCharsets;
 
 public class CryptoManager {
     public static String getSHA256(String message){
-        String hashedMessage = Hashing.sha256().hashString(message, StandardCharsets.UTF_8).toString();
+        HashFunction sha256 = Hashing.sha256();
+        HashCode hashCode = sha256.hashString(message, StandardCharsets.UTF_8);
+        String hashedMessage = hashCode.toString();
         return hashedMessage;
     }
 }
