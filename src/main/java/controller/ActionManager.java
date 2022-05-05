a15be98f7d9804b5949dd0e71af0625555302780
q1e123 <robertnicolaesolca@gmail.com>
2022-05-05 10:47:28 +0300

    merged action manager and action db factory

--- src/main/java/controller/ActionManager.java
+++ src/main/java/controller/ActionManager.java
@@ -1,15 +1,69 @@
 package controller;
 
+import model.ActionArgumentEntity;
+import model.ActionEntity;
+
 public class ActionManager extends DatabaseEntityManager {
-    static private ActionManager actionManager;
+    private static ActionManager actionManager;
+
     private ActionManager(){
         super();
     }
 
-    static public ActionManager getInstance(){
+    public ActionManager getInstance(){
         if (ActionManager.actionManager == null){
             actionManager = new ActionManager();
         }
         return actionManager;
+    }
+    public void addGoTo(Integer routineId, String url){
+        ActionEntity actionEntity = new ActionEntity("GO_TO", routineId);
+        databaseManager.add(actionEntity);
+        ActionArgumentEntity actionArgumentEntity = new ActionArgumentEntity("URL", url, actionEntity.getId());
+        databaseManager.add(actionArgumentEntity);
+    }
+
+    public void addSendKeys(Integer routineId, String xPath, String keys){
+        ActionEntity actionEntity = new ActionEntity("SEND_KEYS", routineId);
+        databaseManager.add(actionEntity);
+        ActionArgumentEntity actionArgumentEntityXPath = new ActionArgumentEntity("XPATH", xPath, actionEntity.getId());
+        ActionArgumentEntity actionArgumentEntityKeys = new ActionArgumentEntity("KEYS", keys, actionEntity.getId());
+        databaseManager.add(actionArgumentEntityXPath);
+        databaseManager.add(actionArgumentEntityKeys);
+    }
+
+    public void addClick(Integer routineId, String xPath){
+        ActionEntity actionEntity = new ActionEntity("CLICK", routineId);
+        databaseManager.add(actionEntity);
+        ActionArgumentEntity actionArgumentEntity = new ActionArgumentEntity("XPATH", xPath, actionEntity.getId());
+        databaseManager.add(actionArgumentEntity);
+    }
+
+    public void addGetText(Integer routineId, String xPath){
+        ActionEntity actionEntity = new ActionEntity("GET_TEXT", routineId);
+        databaseManager.add(actionEntity);
+        ActionArgumentEntity actionArgumentEntity = new ActionArgumentEntity("XPATH", xPath, actionEntity.getId());
+        databaseManager.add(actionArgumentEntity);
+    }
+
+    public void addGetAttribute(Integer routineId, String xPath, String attribute){
+        ActionEntity actionEntity = new ActionEntity("GET_ATTRIBUTE", routineId);
+        databaseManager.add(actionEntity);
+        ActionArgumentEntity actionArgumentEntityXPath = new ActionArgumentEntity("XPATH", xPath, actionEntity.getId());
+        ActionArgumentEntity actionArgumentEntityAttribute = new ActionArgumentEntity("ATTRIBUTE", attribute, actionEntity.getId());
+        databaseManager.add(actionArgumentEntityXPath);
+        databaseManager.add(actionArgumentEntityAttribute);
+    }
+
+    public void addIsSelected(Integer routineId, String xPath) {
+        ActionEntity actionEntity = new ActionEntity("IS_SELECTED", routineId);
+        databaseManager.add(actionEntity);
+        ActionArgumentEntity actionArgumentEntity = new ActionArgumentEntity("XPATH", xPath, actionEntity.getId());
+        databaseManager.add(actionArgumentEntity);
+    }
+
+    public void addCloseBrowser(Integer routineId) {
+        ActionEntity actionEntity = new ActionEntity("CLOSE_BROWSER", routineId);
+        databaseManager.add(actionEntity);
     }
 }
