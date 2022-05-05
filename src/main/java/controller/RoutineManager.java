481d0b5c63366bb07c23335e3582e186c81e54be
q1e123 <robertnicolaesolca@gmail.com>
2022-05-05 12:12:10 +0300

    added getActionsForRoutine

--- src/main/java/controller/RoutineManager.java
+++ src/main/java/controller/RoutineManager.java
@@ -1,6 +1,12 @@
 package controller;
 
+import model.ActionArgumentEntity;
+import model.ActionEntity;
 import model.RoutineEntity;
+import utils.FindUtils;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
 
 public class RoutineManager extends DatabaseEntityManager {
     static private RoutineManager routineManager;
@@ -15,9 +21,25 @@ public class RoutineManager extends DatabaseEntityManager {
         return  routineManager;
     }
 
-    public void addVRoutine(String name, Integer browserId, Integer userId){
+    public void addRoutine(String name, Integer browserId, Integer userId){
         RoutineEntity routineEntity = new RoutineEntity(name, browserId, userId);
         databaseManager.add(routineEntity);
         logger.info("New routine - " + name);
     }
+
+    public Hashtable<String, Hashtable<String, String>> getActionsForRoutine(Integer routineId){
+        Hashtable<String, Hashtable<String, String>> actionTable = new Hashtable<>();
+        ArrayList<ActionEntity> actionEntityList = (ArrayList<ActionEntity>) databaseManager.get("id", routineId, ActionEntity.class);
+        for (ActionEntity action: actionEntityList) {
+            ArrayList<ActionArgumentEntity> argumentList =  (ArrayList<ActionArgumentEntity>) databaseManager.get("actionId", action.getId(), ActionArgumentEntity.class);
+            Hashtable<String, String> argumentTable = new Hashtable<>();
+            for (ActionArgumentEntity argumentEntity : argumentList) {
+                argumentTable.put(argumentEntity.getType(), argumentEntity.getValue());
+            }
+            actionTable.put(action.getName(), argumentTable);
+        }
+        return actionTable;
+    }
+
+
 }
