1ad8d12155f3d215ba8341c4eca45a2f496a3d34
q1e123 <robertnicolaesolca@gmail.com>
2022-05-05 09:47:11 +0300

    modified getchrome

--- src/main/java/controller/WebDriverFactory.java
+++ src/main/java/controller/WebDriverFactory.java
@@ -5,8 +5,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class WebDriverFactory {
-    public static WebDriver getChrome(Boolean incognito, Boolean headless){
-        System.setProperty("webdriver.chrome.driver", "D:/Workshop/Utils/chromedriver-88.exe");
+    public static WebDriver getChrome(Boolean incognito, Boolean headless, String path){
+        System.setProperty("webdriver.chrome.driver", path);
         ChromeOptions chromeOptions = new ChromeOptions();
         if (incognito){
             chromeOptions.addArguments("--incognito");
