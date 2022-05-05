a65c31ee4b7c18b79366aa66205f38bfc5beb130
q1e123 <robertnicolaesolca@gmail.com>
2022-05-05 10:19:23 +0300

    added routine manager

---
+++ src/main/java/controller/RoutineManager.java
@@ -0,0 +1,25 @@
+package controller;
+
+import model.RoutineEntity;
+
+public class RoutineManager {
+    static private RoutineManager routineManager;
+
+    private DatabaseManager databaseManager;
+
+    private RoutineManager(){
+        databaseManager = DatabaseManager.getInstance();
+    }
+
+    static public RoutineManager getInstance(){
+        if (RoutineManager.routineManager == null){
+            RoutineManager.routineManager = new RoutineManager();
+        }
+        return  routineManager;
+    }
+
+    public void addVRoutine(String name, Integer browserId, Integer userId){
+        RoutineEntity routineEntity = new RoutineEntity(name, browserId, userId);
+        databaseManager.add(routineEntity);
+    }
+}
