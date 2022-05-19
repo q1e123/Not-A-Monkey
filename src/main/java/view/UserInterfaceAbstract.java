706f601ad26e8a39559d75e06d2ba3be90c1e819
q1e123 <robertnicolaesolca@gmail.com>
2022-05-19 11:34:00 +0300

    added routine manager

--- src/main/java/view/UserInterfaceAbstract.java
+++ src/main/java/view/UserInterfaceAbstract.java
@@ -3,12 +3,13 @@ package view;
 import controller.AccountManager;
 import controller.ActionManager;
 import controller.BrowserManager;
+import controller.RoutineManager;
 
 public abstract class UserInterfaceAbstract {
     protected AccountManager accountManager = null;
     protected ActionManager actionManager = null;
-
     protected BrowserManager browserManager = null;
+    protected RoutineManager routineManager = null;
     public abstract void run();
 
     protected abstract void createNewAccount();
