7275614d80f3bc10a9706bc2f2b39287561717ad
q1e123 <robertnicolaesolca@gmail.com>
2022-05-20 09:40:40 +0300

    removed debug code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,14 +2,12 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import view.CommandLineInterface;
 
-import java.io.File;
-
 public class Main {
     public static void main(String[] args) {
         Logger logger = LogManager.getLogger(Main.class);
         logger.info("Application has started");
-        String filePath = new File("").getAbsolutePath();
-        filePath.concat("path to the property file");
+        CommandLineInterface commandLineInterface = new CommandLineInterface();
+        commandLineInterface.run();
         logger.info("Application has ended");
     }
 }
