0fed37d96edab8ea3159b3e0e9e79bf7740e4405
q1e123 <robertnicolaesolca@gmail.com>
2022-05-20 09:40:13 +0300

    added ff and tor tests

--- src/test/java/EasyDriverTest.java
+++ src/test/java/EasyDriverTest.java
@@ -20,4 +20,24 @@ public class EasyDriverTest {
         Assertions.assertEquals(expected, actual);
     }
 
+    @Test
+    void firefoxTest(){
+        WebDriver driver = WebDriverFactory.getFireFox(false, false, "C:\\Users\\user\\Downloads\\geckodriver.exe");
+        EasyDriver bot = new EasyDriver(driver);
+        bot.goTo("http://knightrobert.com/");
+        bot.click("//*[@id=\"cs-content\"]/div/div/div/div/div[2]/div/div[2]/a/div/div/span");
+        String expected = "Professional";
+        String actual = bot.getText("//*[@id=\"post-30\"]/div/div/header/h1");
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void torTest(){
+        WebDriver driver = WebDriverFactory.getTor(false, false, "C:\\Users\\user\\Downloads\\geckodriver.exe");
+        EasyDriver bot = new EasyDriver(driver);
+        bot.goTo("https://whatismyipaddress.com/");
+        String expected = "Romania";
+        String actual = bot.getText("//*[@id=\"fl-post-111\"]/div/div/div[1]/div/div[2]/div/div/div/div/div/div[2]/div[1]/div[4]/div/p[4]/span[2]");
+        Assertions.assertNotEquals(expected, actual);
+    }
 }
