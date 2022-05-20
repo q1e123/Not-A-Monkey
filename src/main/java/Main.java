df51b721f79058c526bb651f8fc4c56e18d1f3b9
q1e123 <robertnicolaesolca@gmail.com>
2022-05-20 08:48:55 +0300

    added getFirefox

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,14 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import view.CommandLineInterface;
 
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
         Logger logger = LogManager.getLogger(Main.class);
         logger.info("Application has started");
-        CommandLineInterface commandLineInterface = new CommandLineInterface();
-        commandLineInterface.run();
+        String filePath = new File("").getAbsolutePath();
+        filePath.concat("path to the property file");
         logger.info("Application has ended");
     }
 }
--- src/main/java/controller/WebDriverFactory.java
+++ src/main/java/controller/WebDriverFactory.java
@@ -3,14 +3,34 @@ package controller;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class WebDriverFactory {
     public static WebDriver getDriver(String path, String browserType){
+        WebDriver driver = null;
         if (browserType.equals("GOOGLE_CHROME")){
-            WebDriver driver = getChrome(false, false, path);
+            driver = getChrome(false, false, path);
+            return driver;
+        }
+        if (browserType.equals("FIREFOX")){
+            driver = getFireFox(false, false, path);
             return driver;
         }
         return null;
+    }
+
+    public static WebDriver getFireFox(Boolean incognito, Boolean headless, String path){
+        System.setProperty("webdriver.firefox.driver", path);
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        if (incognito){
+            firefoxOptions.addArguments("--incognito");
+        }
+        if (headless){
+            firefoxOptions.addArguments("--headless");
+        }
+        WebDriver driver = new FirefoxDriver(firefoxOptions);
+        return driver;
     }
 
     public static WebDriver getChrome(Boolean incognito, Boolean headless, String path){
